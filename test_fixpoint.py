e6a6d778efa1ede34da69b24bbfce7486270c919
kawawan <40896306+kawawan@users.noreply.github.com>
2020-05-29 02:49:14 +0900

    Update test_fixpoint.py
    問3まで
    使い方：起動すると入力を求められます。ログファイルのあるディレクトリの絶対パスと、解析期間の開始と終わりの年月日を指示に従って入力します。すると表示されます。
    解析期間の始まりと終わりが逆転していた場合、プログラムが終了します。

--- test_fixpoint.py
+++ test_fixpoint.py
@@ -3,8 +3,9 @@ import pandas as pd
 import locale
 from pathlib import Path
 import numpy as np
+import os
 
-
+print("解析したいログファイルがあるディレクトリの絶対パスを入力してください。")
 dir = input()
 #path = 'C:/Program Files (x86)/Apache/Apache24/logs/test.log'
 p = Path(dir)
@@ -16,6 +17,19 @@ final_times_cnt = np.array([0 for x in range(0,24)])
 final_hostdict = {}
 #print(type(log_list))
 
+print("解析の期間を指定します。")
+print("最初に、いつからのデータを記録したいか、その開始年月日を入力してください。")
+print("なお、フォーマットは %Y/%m/%d で入力してください。")
+start_str = input()
+start_time = datetime.datetime.strptime(start_str, '%Y/%m/%d')
+print("次に、いつまでのデータを記録したいか、その終了年月日を入力してください")
+print("なお、フォーマットは %Y/%m/%d で入力してください。")
+finish_str = input()
+finish_time = datetime.datetime.strptime(finish_str, '%Y/%m/%d')
+if finish_time < start_time:
+    print("開始時間と終了時間が不適切です。")
+    print("最初からやり直してください")
+    exit()
 
 for log_file in log_list:
     with open(log_file) as f:
@@ -25,18 +39,26 @@ for log_file in log_list:
     #    print(type(l))
         times_cnt = [0 for i in range(0,24)]
         access_time_list = []
-        for i in range(len(l)):
+        hostdict = {}
+
+        for i in range(len(l)): # ここでホストと時間帯別同時に見る範囲を決めておく
+            hostname = l[i].split()[0]
             time = l[i].split()[3]
             time = time.strip("[")
-            time = time[-8:]
-            date_dt = datetime.datetime.strptime(time, '%H:%M:%S')
-            access_time_list.append(date_dt)
+            #time = time[-8:]
+            date_dt = datetime.datetime.strptime(time, '%d/%b/%Y:%H:%M:%S')
+            if start_time <= date_dt < finish_time:
+                access_time_list.append(date_dt)
+                if not hostname in hostdict:
+                    hostdict[hostname] = 1
+                else:
+                    hostdict[hostname] += 1
 
     #    print(access_time_list)
 
         a = datetime.timedelta(hours= 1)
         b = datetime.timedelta(seconds=1)
-        start = datetime.datetime(year=1900,month=1,day=1)
+        start = datetime.datetime(year=start_time.year, month=start_time.month, day=start_time.day)
         finish = datetime.datetime(year=1900,month=1,day=1,hour=23)
     #    print(a,b,start,finish)
     #    print(type(start + a * 23),type(date_dt),date_dt)
@@ -55,15 +77,6 @@ for log_file in log_list:
         final_times_cnt = final_times_cnt + times_cnt_np
 
     # host別
-        hostdict = {}
-        for i in range(len(l)):
-            hostname = l[i].split()[0]
-            #print(hostname)
-            if not hostname in hostdict:
-                hostdict[hostname] = 1
-            else:
-                hostdict[hostname] += 1
-
         for k in hostdict:
             if not k in final_hostdict:
                 final_hostdict[k] = hostdict[k]
